49365ff3d938bf498d703a339a445f171dce931c
Tsdebug <taworkspace01@gmail.com>
2026-02-01 01:43:35 +0530

    Added convex client provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar";
+import ConvexClientProvider from "@/components/convex-client-provider";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -23,8 +24,10 @@ export default function RootLayout({
       <body
         className={`${inter.variable} min-h-[calc(100vh-2rem)] flex flex-col gap-4 antialiased`}
       >
-        <Navbar />
-        <main className="px-2 md:px-4 grow flex flex-col">{children}</main>
+        <ConvexClientProvider>
+          <Navbar />
+          <main className="px-2 md:px-4 grow flex flex-col">{children}</main>
+        </ConvexClientProvider>
       </body>
     </html>
   );
