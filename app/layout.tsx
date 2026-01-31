4f8615e19e61d87492e3b6b942917fd392cd5c49
Tsdebug <taworkspace01@gmail.com>
2026-01-31 23:41:25 +0530

    Added navbar component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/navbar";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -22,6 +23,7 @@ export default function RootLayout({
       <body
         className={`${inter.variable} min-h-[calc(100vh-2rem)] flex flex-col gap-4 antialiased`}
       >
+        <Navbar />
         <main className="px-2 md:px-4 grow flex flex-col">{children}</main>
       </body>
     </html>
---
+++ components/navbar.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Navbar() {
+  return (
+    <nav className="w-full h-14 px-4 border-b shadow-sm bg-background flex items-center">
+        <div className="flec items-center w-full justify-between">
+            <div className="flec items-center gap-x-4">
+                <Link href="/">
+                <h1 className="font-semibold text-xl">Todo app</h1>
+                </Link>
+            </div>
+        </div>
+    </nav>
+  )
+}
