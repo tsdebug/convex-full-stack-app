5436558034591747001ad83fc2af279274f5fd32
Tsdebug <taworkspace01@gmail.com>
2026-02-02 21:25:00 +0530

    Modifications for adding todo query and mutation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
-import Image from "next/image";
+import CreateTodo from "@/components/todos/create-todo";
+import TodoList from "@/components/todos/todo-list";
 
-export default function Home() {
-  return (
-    <div>Hello world !</div>
-  );
+export default function Todos() {
+	return (
+		<div className="w-full max-w-3xl mx-auto flex flex-col gap-4">
+			<CreateTodo />
+			<TodoList />
+		</div>
+	);
 }
