33a29128fb55a5662f5b068eef045fa4740ebee5
Tsdebug <taworkspace01@gmail.com>
2026-02-03 10:39:40 +0530

    removed .shape which was causing problem in the new zod versoon

--- convex/todos.ts
+++ convex/todos.ts
@@ -1,5 +1,9 @@
-import { v } from "convex/values";
 import { mutation, query } from "./_generated/server";
+import { zCustomMutation } from "convex-helpers/server/zod3";
+import { NoOp } from "convex-helpers/server/customFunctions"; // NoOp means - we dont want to change the input in
+import { createTodoSchema} from "../lib/zod";
+
+const zMutation = zCustomMutation(mutation, NoOp);
 
 // for the query function the args is not needed 
 export const getTodos = query({
@@ -13,11 +17,8 @@ export const getTodos = query({
 });
 
 // for the mutation function the args is needed
-export const createTodo = mutation({
-    args: {
-        title: v.string(),
-        completed: v.boolean(),
-    },
+export const createTodo = zMutation({
+    args: createTodoSchema,
     handler: async (ctx, args) => {
         return ctx.db.insert("todos", {
             title: args.title,
