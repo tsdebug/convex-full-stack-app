7cdc876b49c92b755a22540d721759b4e3e478d0
Tsdebug <taworkspace01@gmail.com>
2026-02-03 11:38:16 +0530

    Added update and delete zod schema

--- lib/zod.ts
+++ lib/zod.ts
@@ -1,6 +1,18 @@
+import { zid } from "convex-helpers/server/zod3";
 import { z } from "zod";
 
 export const createTodoSchema = z.object({
     title: z.string().min(3, { message: "Title must be at least 3 characters long" }),
     completed: z.boolean(),
+});
+
+export const updateTodoSchema = z.object({
+    // INFO: zid does not do table name validation, it is just going to check if the id is a string
+    id: zid ("todos"),
+    title: z.string().min(3, { message: "Title must be at least 3 characters long" }),
+    completed: z.boolean(),
+});
+
+export const deleteTodoSchema = z.object({
+    id: zid("todos"),
 });
