27037f8fa86d19a0e178d5e369b6aee26aa11d09
Tsdebug <taworkspace01@gmail.com>
2026-02-03 11:37:49 +0530

    Added update and delete function

--- convex/todos.ts
+++ convex/todos.ts
@@ -1,11 +1,12 @@
 import { mutation, query } from "./_generated/server";
 import { zCustomMutation } from "convex-helpers/server/zod3";
 import { NoOp } from "convex-helpers/server/customFunctions"; // NoOp means - we dont want to change the input in
-import { createTodoSchema} from "../lib/zod";
+import { createTodoSchema, deleteTodoSchema, updateTodoSchema} from "../lib/zod";
 
 const zMutation = zCustomMutation(mutation, NoOp);
 
 // for the query function the args is not needed 
+// to get the todos
 export const getTodos = query({
     handler: async (ctx) => {
         const todos = await ctx.db
@@ -17,6 +18,7 @@ export const getTodos = query({
 });
 
 // for the mutation function the args is needed
+// to create a todo
 export const createTodo = zMutation({
     args: createTodoSchema,
     handler: async (ctx, args) => {
@@ -24,5 +26,24 @@ export const createTodo = zMutation({
             title: args.title,
             completed: false,
         })
+    },
+})
+
+// mutation to update a todo
+export const updateTodo = zMutation({
+    args: updateTodoSchema,
+    handler: async (ctx, args)=>{
+        return ctx.db.patch(args.id,{
+            title: args.title,
+            completed: args.completed,
+        })
+    }
+});
+
+// mutation to delete a todo
+export const deleteTodo = zMutation({
+    args: deleteTodoSchema,
+    handler: async (ctx, args)=>{
+        return ctx.db.delete(args.id);
     },
 })
